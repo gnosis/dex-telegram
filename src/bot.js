f8d5229f7eb48e6f706795ecb11e5c341502a59f
Anxo Rodriguez <anxolin@gmail.com>
2019-08-30 18:51:11 +0200

    Log message and write into channel

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,16 @@
 const assert = require('assert')
 const TelegramBot = require('node-telegram-bot-api')
 
+const debug = require('debug')('DEBUG-bot')
+const info = require('debug')('INFO-bot')
+
 require('dotenv').config()
 
 const token = process.env.TELEGRAM_TOKEN
+const channelId = process.env.TELEGRAM_CHANNEL_ID
+
 assert(token, "TELEGRAM_TOKEN env var is required")
+assert(channelId, "TELEGRAM_CHANNEL_ID env var is required")
 
 const MESSAGE_GO_TO_CHANNEL = `Hey, for now I prefer to talk in dFusion channel.
 
@@ -21,15 +27,24 @@ const bot = new TelegramBot(token, {
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id
+  // const chatId = msg.chat.id
   const resp = match[1] // the captured "whatever"
 
-  bot.sendMessage(chatId, resp)
+  debug("Received an echo message: %s, answering in 3s. %s, %o", resp, msg, match)
+  const msg = 'This is a new order: ' + resp
+  debug("Writtin into the channel", msg)
+  bot.sendMessage(channelId, msg)
 })
+
+
+
 
 // Listen to any message
 bot.on('message', (msg) => {
   const chatId = msg.chat.id
+  debug("Received msg: %o", msg)
 
   bot.sendMessage(chatId, MESSAGE_GO_TO_CHANNEL)
 })
+
+info("The bot is up :)")
