87132128e496f6915b4611368750c89924a4df2f
Anxo Rodriguez <anxolin@gmail.com>
2019-11-09 11:37:57 +0100

    Define the recomended  setup for typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'eslint:recommended', // set of rules which are recommended for all projects by the ESLint Team
+    'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended', // Adjust the one from eslint appropriately for TypeScript
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
