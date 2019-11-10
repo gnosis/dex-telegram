ab7e06db0df00d9639f7056f457561c1f63ef9c0
Anxo Rodriguez <anxolin@gmail.com>
2019-11-10 12:49:36 +0100

    Print encoded functions

---
+++ test/sandbox/printEncodedFunctions.ts
@@ -0,0 +1,23 @@
+import { web3 } from '../../src/helpers/web3'
+import Logger from '../../src/helpers/Logger'
+import { AbiItem } from 'web3-utils'
+
+require('dotenv').config()
+
+/**
+ *  SANDBOX: Get accounts from connected wallet
+ *  RUN:     yarn sandbox test/sandbox/printEncodedFunctions.ts
+ */
+const log = new Logger('sandbox:printEncodedFunctions')
+
+async function exec (): Promise<void> {
+  const abi = require('../../src/contracts/StablecoinConverter.json')
+  const functions = abi.filter((def: AbiItem) => def.type === 'function')
+
+  log.info('Found %d functions:', functions.length)
+  functions.forEach((def: AbiItem) => {
+    log.info('  - %s: ', def.name, web3.eth.abi.encodeFunctionSignature(def))
+  })
+}
+
+exec().catch(log.error)
