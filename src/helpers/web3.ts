d9c501887ad5e2b24308420410588515c58903af
Anxo Rodriguez <anxolin@gmail.com>
2019-11-12 11:26:15 +0100

    Add get past logs

--- src/helpers/web3.ts
+++ src/helpers/web3.ts
@@ -7,5 +7,4 @@ const nodeUrl = process.env.NODE_URL
 
 log.info('Connecting to ethereum using web3: %s', nodeUrl)
 assert(nodeUrl, 'NODE_URL env var is required')
-export const provider = 'ws://localhost:8545'
-export const web3: Web3 = new Web3(provider)
+export const web3: Web3 = new Web3(nodeUrl as string)
