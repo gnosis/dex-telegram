a40c9221ac482240e7d8b3e14edfb27f2debea62
Anxo Rodriguez <anxolin@gmail.com>
2019-11-12 11:34:53 +0100

    Create repo factory

---
+++ src/repos/index.ts
@@ -0,0 +1,14 @@
+import { DfusionRepo } from 'types'
+import DfusionRepoImpl from './dfusion/DfusionRepoImpl'
+import { stableCoinConverterContract } from '../contracts'
+import { web3 } from '../helpers/web3'
+
+function createDfusionRepo (): DfusionRepo {
+  return new DfusionRepoImpl({
+    stableCoinConverterContract,
+    web3
+  })
+}
+
+// Build Repos
+export const dfusionRepo = createDfusionRepo()
