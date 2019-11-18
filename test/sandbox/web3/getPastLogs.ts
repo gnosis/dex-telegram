0388d9290925db88805edb0b147751b6b9623818
Anxo Rodriguez <anxolin@gmail.com>
2019-11-18 20:47:38 +0100

    Add a new sanbox to get the past deposits from the contract

--- test/sandbox/web3/getPastLogs.ts
+++ test/sandbox/web3/getPastLogs.ts
@@ -15,7 +15,7 @@ async function exec (): Promise<void> {
   assert(contractAddress, 'STABLE_COIN_CONTRACT_ADDRESS is required env var')
   const topics = [
     // OrderPlacement
-    '0x951b66125eb3ef23450a52bb65cf4786c423cc944abb7de8a749a7d05642060e',
+    // '0x951b66125eb3ef23450a52bb65cf4786c423cc944abb7de8a749a7d05642060e',
 
     // OrderCancelation
     // '0x36adb26c9fa7b5219b043ac78b698e6e539a937ff88758ab51b04d331e7111ed'
