bf88cb03f98eda70241e23bf7c94c4d4bc495d54
Anxo Rodriguez <anxolin@gmail.com>
2019-11-25 10:57:31 +0100

    Add token details

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,10 @@
 export type Command = () => void
+
+export interface TokenDetails {
+  name?: string
+  symbol?: string
+  decimals?: number
+  address: string
+  addressMainnet?: string
+  image?: string
+}
