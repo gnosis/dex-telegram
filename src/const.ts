4ed12881bba5ed5c08e4aca4bf107af312038ae5
Anxo Rodriguez <anxolin@gmail.com>
2019-11-25 10:48:35 +0100

    Add some utils

---
+++ src/const.ts
@@ -0,0 +1,22 @@
+import BN from 'bn.js'
+
+// Some convenient numeric constant
+export const ZERO = new BN(0)
+export const ONE = new BN(1)
+export const TWO = new BN(2)
+export const TEN = new BN(10)
+
+// Max allowance value for ERC20 approve
+export const ALLOWANCE_MAX_VALUE = new BN(2).pow(new BN(256)).sub(ONE) // 115792089237316195423570985008687907853269984665640564039457584007913129639935
+
+// Arbitrarily big number for checking if the token is enabled
+export const ALLOWANCE_FOR_ENABLED_TOKEN = new BN(2).pow(new BN(128)) // 340282366920938463463374607431768211456
+
+// Model constants
+export const FEE_DENOMINATOR = 1000 // Fee is 1/fee_denominator i.e. 1/1000 = 0.1%
+export const BATCH_TIME = 300
+export const DEFAULT_ORDER_DURATION = 6 // every batch takes 5min, we want it to be valid for 30min, ∴ 30/5 == 6
+
+// UI constants
+export const HIGHLIGHT_TIME = 5000
+export const FEE_PERCENTAGE = (1 / FEE_DENOMINATOR) * 100 // syntatic sugar for displaying purposes
---
+++ src/utils/format.ts
@@ -0,0 +1,134 @@
+import BN from 'bn.js'
+import { TEN } from 'const'
+import { TokenDetails } from 'types'
+
+const DEFAULT_DECIMALS = 4
+const ELLIPSIS = '...'
+
+function _getLocaleSymbols (): { thousands: string; decimals: string } {
+  // Check number representation in default locale
+  const formattedNumber = new Intl.NumberFormat(undefined).format(1000.1)
+  return {
+    thousands: formattedNumber[1],
+    decimals: formattedNumber[5]
+  }
+}
+const { thousands: THOUSANDS_SYMBOL, decimals: DECIMALS_SYMBOL } = _getLocaleSymbols()
+
+function _formatNumber (num: string): string {
+  return num.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1' + THOUSANDS_SYMBOL)
+}
+
+function _decomposeBn (amount: BN, amountPrecision: number, decimals: number): { integerPart: BN; decimalPart: BN } {
+  // Discard the decimals we don't need
+  //  i.e. for WETH (precision=18, decimals=4) --> amount / 1e14
+  //        16.5*1e18 ---> 165000
+  if (decimals > amountPrecision) {
+    throw new Error('The decimals cannot be bigger than the precision')
+  }
+  const amountRaw = amount.divRound(TEN.pow(new BN(amountPrecision - decimals)))
+  const integerPart = amountRaw.div(TEN.pow(new BN(decimals))) // 165000 / 10000 = 16
+  const decimalPart = amountRaw.mod(TEN.pow(new BN(decimals))) // 165000 % 10000 = 5000
+
+  // Discard the decimals we don't need
+  //  i.e. for WETH (precision=18, decimals=4) --> amount / 1e14
+  //        1, 18:  16.5*1e18 ---> 165000
+
+  return { integerPart, decimalPart }
+}
+
+export function formatAmount (
+  amount: BN | null | undefined,
+  amountPrecision: number,
+  decimals = DEFAULT_DECIMALS,
+  thousandSeparator: boolean = true
+): string | null {
+  if (!amount) {
+    return null
+  }
+  const actualDecimals = Math.min(amountPrecision, decimals)
+  const { integerPart, decimalPart } = _decomposeBn(amount, amountPrecision, actualDecimals)
+
+  const integerPartFmt = thousandSeparator ? _formatNumber(integerPart.toString()) : integerPart.toString()
+
+  if (decimalPart.isZero()) {
+    // Return just the integer part
+    return integerPartFmt
+  } else {
+    const decimalFmt = decimalPart
+      .toString()
+      .padStart(actualDecimals, '0') // Pad the decimal part with leading zeros
+      .replace(/0+$/, '') // Remove the right zeros
+
+    // Return the formated integer plus the decimal
+    return integerPartFmt + DECIMALS_SYMBOL + decimalFmt
+  }
+}
+
+export function formatAmountFull (
+  amount: BN | null | undefined,
+  amountPrecision: number,
+  thousandSeparator: boolean = true
+): string | null {
+  if (!amount) {
+    return null
+  }
+
+  return formatAmount(amount, amountPrecision, amountPrecision, thousandSeparator)
+}
+
+/**
+ * Adjust the decimal precision of the given decimal value, without converting to/from BN or Number
+ * Takes in a string and returns a string
+ *
+ * E.g.:
+ * adjustPrecision('1.2657', 3) === '1.265'
+ *
+ * @param value The decimal value to be adjusted as a string
+ * @param precision How many decimals should be kept
+ */
+export function adjustPrecision (value: string | undefined | null, precision: number): string {
+  if (!value) {
+    return ''
+  }
+
+  const regexp = new RegExp(`(\\.\\d{${precision}})\\d+$`)
+  return value.replace(regexp, '$1')
+}
+
+export function parseAmount (amountFmt: string, amountPrecision: number): BN | null {
+  if (!amountFmt) {
+    return null
+  }
+
+  const adjustedAmount = adjustPrecision(amountFmt, amountPrecision)
+  const groups = /^(\d+)(?:\.(\d+))?$/.exec(adjustedAmount)
+  if (groups) {
+    const [, integerPart, decimalPart = ''] = groups
+
+    const decimalBN = new BN(decimalPart.padEnd(amountPrecision, '0'))
+    const factor = TEN.pow(new BN(amountPrecision))
+    return new BN(integerPart).mul(factor).add(decimalBN)
+  } else {
+    return null
+  }
+}
+
+export function abbreviateString (inputString: string, prefixLength: number, suffixLength: number = 0): string {
+  // abbreviate only if it makes sense, and make sure ellipsis fits into word
+  // 1. always add ellipsis
+  // 2. do not shorten words in case ellipsis will make the word longer
+  // 3. min prefix == 1
+  // 4. add suffix if requested
+  const _prefixLength = Math.max(1, prefixLength)
+  if (inputString.length < _prefixLength + ELLIPSIS.length + suffixLength) {
+    return inputString
+  }
+  const prefix = inputString.slice(0, _prefixLength)
+  const suffix = suffixLength > 0 ? inputString.slice(-suffixLength) : ''
+  return prefix + ELLIPSIS + suffix
+}
+
+export function safeTokenName (token: TokenDetails): string {
+  return token.symbol || token.name || abbreviateString(token.address, 6, 4)
+}
