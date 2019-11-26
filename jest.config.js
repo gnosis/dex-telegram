859a2d98936549559f6ea2031689364155c06eee
Velenir <velenir@gmail.com>
2019-11-26 15:26:07 +0300

    different ignore pattern

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: ['/node_modules/', '/build/', '.*'],
-  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dex-telegram/build/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
