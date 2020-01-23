c58e19f945e60d351fe8b5c4d04f8c8990cd7e93
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-01-23 15:43:42 -0800

    Using unlimited methods from dex-js on new order message

--- src/helpers/message.ts
+++ src/helpers/message.ts
@@ -1,7 +1,13 @@
 import BN from 'bn.js'
 import moment from 'moment-timezone'
 
-import { FEE_DENOMINATOR, formatAmountFull, formatAmount } from '@gnosis.pm/dex-js'
+import {
+  FEE_DENOMINATOR,
+  formatAmountFull,
+  formatAmount,
+  isOrderUnlimited,
+  isNeverExpiresOrder,
+} from '@gnosis.pm/dex-js'
 import BigNumber from 'bignumber.js'
 import { TokenDto } from 'services'
 
@@ -61,6 +67,9 @@ export function newOrderMessage(order: any, baseUrl: string): string {
     buyToken,
   )
 
+  // unlimited?
+  const isUnlimited = isOrderUnlimited(priceNumerator, priceDenominator)
+
   // Format the amounts
   // TODO: Allow to use BN, string or BigNumber or all three in the format. Review in dex-js
   const fillSellAmountFmt = formatAmountFull(
@@ -71,7 +80,15 @@ export function newOrderMessage(order: any, baseUrl: string): string {
 
   // TODO: Should we publish even if the user doesn't have balance. Should we include the balance of the user? he can change it...
   //  https://github.com/gnosis/dex-telegram/issues/45
-  let message = `Sell *${sellAmountFmt}* \`${sellTokenLabel}\` for *${buyAmountFmt}* \`${buyTokenLabel}\`\n`
+  let sellMsg: string
+  if (isUnlimited) {
+    sellMsg = `Sell \`${sellTokenLabel}\` for \`${buyTokenLabel}\``
+  } else {
+    sellMsg = `Sell *${sellAmountFmt}* \`${sellTokenLabel}\` for *${buyAmountFmt}* \`${buyTokenLabel}\`\n`
+  }
+
+  let message = sellMsg
+
   message += `\n  - *Price*:  1 \`${sellTokenLabel}\` = ${price} \`${buyTokenLabel}\``
 
   // Only display the valid from if the period hasn't started
@@ -80,12 +97,35 @@ export function newOrderMessage(order: any, baseUrl: string): string {
     // The order is not active yet
     message += `\n  - *Tradable*: \`${moment(validFrom).calendar()} GMT\`, \`${moment(validFrom).fromNow()}\``
   }
-  message += `\n  - *Expires*: \`${moment(validUntil).calendar()} GMT\`, \`${moment(validUntil).fromNow()}\``
+
+  // Does it expire?
+  const isNeverExpires = isNeverExpiresOrder(validUntil)
+
+  let expirationMsg: string
+  if (isNeverExpires) {
+    expirationMsg = '\n  - *Expires*: Valid until cancelled'
+  } else {
+    expirationMsg = `\n  - *Expires*: \`${moment(validUntil).calendar()} GMT\`, \`${moment(validUntil).fromNow()}\``
+  }
+  message += expirationMsg
+
   if (!sellToken.known || !buyToken.known) {
     message +=
-      '\n  - "Maybe" means one or more tokens claim to be called as shown, but it\'s not currently part of the list of [known tokens](https://github.com/gnosis/dex-js/blob/master/src/tokenList.json). Make sure you verify the address yourself before trading against it.'
+      '\n  - "Maybe" means one or more tokens claim to be called as shown, ' +
+      "but it's not currently part of the list of [known tokens](https://github.com/gnosis/dex-js/blob/master/src/tokenList.json). " +
+      'Make sure you verify the address yourself before trading against it.'
   }
-  message += `\n\nFill the order here: ${baseUrl}/trade/${buyTokenParam}-${sellTokenParam}?sell=${fillSellAmountFmt}&buy=${buyAmountFullFmt}`
+
+  let fillAmountBuy: string
+  let fillAmountSell: string
+  if (isUnlimited) {
+    fillAmountBuy = price
+    fillAmountSell = '1'
+  } else {
+    fillAmountBuy = buyAmountFullFmt as string
+    fillAmountSell = fillSellAmountFmt as string
+  }
+  message += `\n\nFill the order here: ${baseUrl}/trade/${buyTokenParam}-${sellTokenParam}?sell=${fillAmountSell}&buy=${fillAmountBuy}`
 
   return message
 }
