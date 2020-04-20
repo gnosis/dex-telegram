f9bf1d26af1c315cf1c3e7dc721b3d72a1451421
Velenir <velenir@gmail.com>
2020-04-20 18:42:34 +0300

    tentative BufferedBot implementation

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,15 @@
 import moment from 'moment-timezone'
 import TelegramBot, { Message, User } from 'node-telegram-bot-api'
+import { Subject, timer, from } from 'rxjs'
+import { bufferTime, filter, groupBy, mergeMap, concatMap, ignoreElements, startWith, pluck, scan } from 'rxjs/operators'
 
 import Server from 'Server'
 import { Logger, logUnhandledErrors, onShutdown, assert } from '@gnosis.pm/dex-js'
 
 import { dfusionService } from 'services'
-import { newOrderMessage } from 'helpers'
+import { newOrderMessage, SendMessageInput, MESSAGE_DELIMITER } from 'helpers'
 
-const WEB_BASE_URL = process.env.WEB_BASE_URL
+const WEB_BASE_URL = process.env.WEB_BASE_URL || ''
 assert(WEB_BASE_URL, 'WEB_BASE_URL is required')
 const port = parseInt(process.env.API_PORT || '3000')
 
@@ -26,7 +28,71 @@ assert(channelId, 'TELEGRAM_CHANNEL_ID env var is required')
 const isPublicChannel = isNaN(channelId as any)
 const channelHandle = isPublicChannel ? channelId : '**private chat**'
 
-const bot = new TelegramBot(token, {
+class BufferedBot extends TelegramBot {
+  private messagesSubject$ = new Subject<SendMessageInput>()
+
+  constructor(token: string, options?: TelegramBot.ConstructorOptions) {
+    super(token, options)
+
+    this.messagesSubject$.asObservable().pipe(
+      // group messages by chatId
+      groupBy(messageInput => messageInput.chatId),
+      mergeMap(group$ => group$.pipe(
+        // buffer messages for 3sec
+        bufferTime(6000),
+        // pass forth only non-empty arrays
+        filter(array => array.length > 0),
+      )),
+      // here all messages belong to the same chatId
+      mergeMap(messageIputArray => {
+        return from(messageIputArray).pipe(
+          // concatenate messages in pieces no longer than 4096 characters
+          scan((accum, message, index) => {
+            const concatText = accum.message.text + (index > 0 ? MESSAGE_DELIMITER : '') + message.text
+            if (concatText.length < 4096) { // avoids Error: Message is too long
+              accum.message.text = concatText
+              accum.pass = null
+            } else {
+              accum.pass = accum.message
+              accum.message = message
+            }
+
+            if (index === messageIputArray.length - 1) {
+              accum.pass = accum.message
+            }
+            return accum
+          }, { message: { ...messageIputArray[0], text: '' }, pass: null } as { message: SendMessageInput, pass: SendMessageInput | null }),
+          pluck('pass'),
+          filter(Boolean),
+        )
+      }),
+      // if multiple compound messages coming through
+      // space them out by 1sec
+      concatMap(compoundMessage => timer(1000).pipe(
+        ignoreElements(),
+        startWith(compoundMessage),
+      )),
+    ).subscribe(compoundMessage => {
+      console.log('compoundMessage', compoundMessage)
+
+      const { chatId, text, options } = compoundMessage
+      console.log('text', text.length)
+
+      super.sendMessage(chatId, text, options)
+    })
+  }
+
+  // have to return any to respect TelegramBot.sendMessage type
+  sendMessage(chatId: number | string, text: string, options?: TelegramBot.SendMessageOptions): any {
+    this.messagesSubject$.next({
+      chatId,
+      text,
+      options,
+    })
+  }
+}
+
+const bot = new BufferedBot(token, {
   polling: true,
 })
 
@@ -113,6 +179,7 @@ Also, here are some links you might find useful:
 dfusionService.watchOrderPlacement({
   onNewOrder(order) {
     const message = newOrderMessage(order, WEB_BASE_URL)
+    // console.log('message', message)
     // Send message
     bot.sendMessage(channelId, message, { parse_mode: 'Markdown' })
   },
