f41299fe27e366647a62cbbf9da4c38efe755092
Velenir <velenir@gmail.com>
2020-04-22 18:30:19 +0300

    add retry logic

--- src/bufferedBot.ts
+++ src/bufferedBot.ts
@@ -1,6 +1,27 @@
 import TelegramBot from 'node-telegram-bot-api'
-import { Subject, timer } from 'rxjs'
-import { bufferTime, filter, groupBy, mergeMap, concatMap, ignoreElements, startWith } from 'rxjs/operators'
+import {
+  Subject,
+  of,
+  timer,
+  EMPTY,
+  throwError,
+} from 'rxjs'
+import {
+  bufferTime,
+  filter,
+  groupBy,
+  mergeMap,
+  concatMap,
+  switchMap,
+  ignoreElements,
+  startWith,
+  retryWhen,
+  delayWhen,
+  delay,
+  map,
+  tap,
+  catchError,
+} from 'rxjs/operators'
 
 import { SendMessageInput, concatMessages } from 'helpers'
 import { Logger } from '@gnosis.pm/dex-js'
@@ -9,6 +30,10 @@ const log = new Logger('bot:buffered')
 
 const BUFFER_TIME = 3000 // consecutive messages over this time get buffered
 const SPACE_TIME = 1000 // min time between sending messages
+const MAX_RETRIES = 5 // give up on a message after so many retries
+
+const RETRY_IN_REGEXP = /\bretry after (\d+)/ // to extract recommended delay from
+// 'Too Many Requests: retry in N' error message
 
 // sendMessage buffers messages and sends them in batches
 export class BufferedBot extends TelegramBot {
@@ -37,14 +62,55 @@ export class BufferedBot extends TelegramBot {
       concatMap(compoundMessage => timer(SPACE_TIME).pipe(
         ignoreElements(),
         startWith(compoundMessage),
+        mergeMap(compoundMessage => {
+          log.debug('Sending compound message %o', compoundMessage)
+
+          const { chatId, text, options } = compoundMessage
+
+          return super.sendMessage(chatId, text, { ...options }) // {...hack} because bot mutates options
+        }),
+        retryWhen(error$ =>
+          error$.pipe(
+            switchMap((error: Error, index) => {
+              // on MAX_RETRIES error -- skip this unfortunate message
+              log.error('Error sending message:', error.message)
+              if (index === MAX_RETRIES) {
+                log.error('Maximum retries of', MAX_RETRIES, 'reached')
+                log.error('Message %o will not be sent', compoundMessage)
+                // rethrow error
+                return throwError(error)
+              }
+
+              // pass the error on
+              return of(error)
+            }),
+            map(error => {
+              // if error is 'Too Many Requests: retry in N'
+              const errorMatch = error.message.match(RETRY_IN_REGEXP)
+              const delaySec = errorMatch ? parseInt(errorMatch[1], 10) : SPACE_TIME
+              // or in SPACE_TIME seconds
+
+              log.error('Retrying in', delaySec, 'seconds')
+
+              return delaySec
+            }),
+            // delay between retries
+            delayWhen(delaySec => timer(delaySec * 1000)),
+            tap(() => log.error('Retrying now'), e => console.log('ErrorError', !!e), () => console.log('COMPLETE complete')),
+            // catch rethrown error
+            // and close pipe for this message
+            // after giving some delay before next compoundMessage
+            // on top of usual spacing
+            catchError(() => EMPTY.pipe(delay(SPACE_TIME))),
+          ),
+        ),
       )),
-    ).subscribe(compoundMessage => {
-      log.debug('Sending compound message %o', compoundMessage)
-
-      const { chatId, text, options } = compoundMessage
-
-      super.sendMessage(chatId, text, options)
-    })
+    ).subscribe(() => {
+      log.debug('Message sent successfully')
+    },
+    log.errorHandler,
+    () => console.log('COMPLETE'),
+    )
   }
 
   // have to return any to respect TelegramBot.sendMessage type
