db30c6c6f253157b5b60acafc2497ef2aeb5811a
Velenir <velenir@gmail.com>
2020-04-22 18:26:29 +0300

    shorter block range

--- src/services/DfusionService.ts
+++ src/services/DfusionService.ts
@@ -146,7 +146,7 @@ export class DfusionRepoImpl implements DfusionService {
 
   public watchOrderPlacement(params: WatchOrderPlacementParams) {
     this._contract.events
-      .OrderPlacement({ fromBlock: 6348195 }) // allows to get a bunch of past orders at once
+      .OrderPlacement({ fromBlock: 6360000 }) // allows to get a bunch of past orders at once
       .on('connected', subscriptionId => {
         log.debug('Starting to listen for new orders. SubscriptionId: %s', subscriptionId)
       })
