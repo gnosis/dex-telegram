3f78ef5b464dcab633330c2817db47a215b10aec
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-05-06 09:17:02 -0700

    TCR_CONTRACT_ADDRESS env var is required

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const TCR_CONTRACT_ADDRESS = process.env.TCR_CONTRACT_ADDRESS || '0xBb840456546496E7640DC09ba9fE06E67C157E1b'
+export const TCR_CONTRACT_ADDRESS = process.env.TCR_CONTRACT_ADDRESS
 export const TCR_LIST_ID = Number(process.env.TCR_LIST_ID) || 0
 export const TCR_CACHE_TIME = 5 * 60 * 1000
